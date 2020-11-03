e85aca059f5b554dd1afd51b7f0571309b32ff41
YH_macbookpro <8016471+Y-kyoto@users.noreply.github.com>
2020-11-03 21:51:20 +0900

    色変更と週設定の追加

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 'use strict';
 
-function createIcon(hours) {
-  function createCanvas(iconsize) {
+function createIcon(hours, fontColor) {
+  function createCanvas(iconsize, fontColor) {
     var canvas = document.createElement('canvas');
     canvas.setAttribute('width', iconsize);
     canvas.setAttribute('height', iconsize);
@@ -9,10 +9,11 @@ function createIcon(hours) {
     context.font = 'bold ' + (iconsize * 0.7) + 'px \'Meirio\', \'Ubuntu\', \'Lucida Grande\',';
     context.textAlign = 'center';
     context.textBaseline = 'middle';
+    context.fillStyle = fontColor;
     context.fillText(hours, iconsize/2, iconsize/3);
     return context.getImageData(0, 0, iconsize, iconsize);
   }
-  return {16: createCanvas(16), 48: createCanvas(48)};
+  return {16: createCanvas(16, fontColor), 48: createCanvas(48, fontColor)};
 }
 
 function update() {
@@ -20,27 +21,41 @@ function update() {
   var start = new Date(now.getFullYear(), 0, 0);
   var diffTime = now - start;
   var oneDay = 1000 * 60 * 60 * 24;
+  var fontColor = "black";
 
   // 背景色の設定
   chrome.browserAction.setBadgeBackgroundColor({color: '#222'});
-
+  // 文字色の設定
+  chrome.storage.sync.get('useWhiteColor', function(settings) {
+    if(settings.useWhiteColor) fontColor = "white";
+  });
   // 残り％が選択されている時
   chrome.storage.sync.get('isPercents', function(settings) {
     if(settings.isPercents) {
       var diff = Math.floor((365.0 - diffTime / oneDay) * 100.0 / 365.0);
       var timeUnit = "%";
       chrome.browserAction.setBadgeText({text: "残り" + String(timeUnit)});
-      chrome.browserAction.setIcon({imageData: createIcon(diff)});
+      chrome.browserAction.setIcon({imageData: createIcon(diff, fontColor)});
     }
   });
-
   // 残り日数が選択されている時
   chrome.storage.sync.get('isDays', function(settings) {
     if(settings.isDays) {
       var diff = Math.floor((366.0 - diffTime / oneDay));
       var timeUnit = "日";
+      if(settings.useWhiteColor) fontColor = "white";
       chrome.browserAction.setBadgeText({text: "残り" + String(timeUnit)});
-      chrome.browserAction.setIcon({imageData: createIcon(diff)});
+      chrome.browserAction.setIcon({imageData: createIcon(diff, fontColor)});
+    }
+  });
+  // 残り時間が選択されている時
+  chrome.storage.sync.get('isWeeks', function(settings) {
+    if(settings.isWeeks) {
+      var diff = Math.floor((366.0 - diffTime / oneDay) / 7);
+      var timeUnit = "週";
+      if(settings.useWhiteColor) fontColor = "white";
+      chrome.browserAction.setBadgeText({text: "残り" + String(timeUnit)});
+      chrome.browserAction.setIcon({imageData: createIcon(diff, fontColor)});
     }
   });
 }
--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 'use strict';
+// ----------------------------------------
+var radioSelectType = document.querySelector('div[id=selectarea]');
+var colorCheckButton = document.querySelector('div[id=selectcolor]');
 
-var radiobutton = document.querySelector('div[id=selectarea]');
 var isDays = document.querySelector('input[value=isDays]');
 var isPercents = document.querySelector('input[value=isPercents]');
+var isWeeks = document.querySelector('input[value=isWeeks]');
+var useWhiteColor = document.querySelector('input[value=useWhiteColor]');
 
+// ----------------------------------------
 chrome.storage.sync.get('isDays', function(settings) {
   isDays.checked = !!settings.isDays;
   document.querySelector('#loading').setAttribute('done', 'true');
 });
-
 chrome.storage.sync.get('isPercents', function(settings) {
   isPercents.checked = !!settings.isPercents;
+  document.querySelector('#loading').setAttribute('done', 'true');
+});
+chrome.storage.sync.get('isWeeks', function(settings) {
+  isWeeks.checked = !!settings.isWeeks;
+  document.querySelector('#loading').setAttribute('done', 'true');
+});
+chrome.storage.sync.get('useWhiteColor', function(settings) {
+  useWhiteColor.checked = !!settings.useWhiteColor;
   document.querySelector('#loading').setAttribute('done', 'true');
 });
 
@@ -21,8 +33,24 @@ chrome.storage.onChanged.addListener(function(changes) {
   if(changes.isPercents){
     isPercents.checked = changes.isPercents.newValue;
   }
+  if(changes.isWeeks){
+    isWeeks.checked = changes.isWeeks.newValue;
+  }
+  if(changes.useWhiteColor){
+    useWhiteColor.checked = changes.useWhiteColor.newValue;
+  }
 });
 
-radiobutton.onchange = function() {
-  chrome.storage.sync.set({isDays: isDays.checked, isPercents: isPercents.checked});
+// ----------------------------------------
+radioSelectType.onchange = function() {
+  chrome.storage.sync.set({
+    isDays: isDays.checked,
+    isPercents: isPercents.checked,
+    isWeeks: isWeeks.checked,
+  });
+};
+colorCheckButton.onchange = function() {
+  chrome.storage.sync.set({
+    useWhiteColor: useWhiteColor.checked,
+  });
 };
